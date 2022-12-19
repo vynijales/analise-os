f2a92a4e8c38c179b51029fb318261a27eca4a8f
vynijales <vynijales@gmail.com>
2022-12-19 10:42:10 -0300

    Implementando & c:/Users/Matheus/Documents/Programação/att_script/venv/Scripts/python.exe c:/Users/Matheus/Documents/Programação/att_script/graphic_interface.py

--- graphic_interface.py
+++ graphic_interface.py
@@ -1,21 +1,35 @@
 from tkinter import *
+import pyperclip as pc
+import datetime
 
 obs = ""
-comeco = ""
+setor = "N2: "
+user = ""
 fim = ""
 
 def atualizar():
-    obs = comeco + fim
+    global user
+
+    user = ent_user.get()
+
+    obs = setor + fim + "\n\n" + user + rodape
+
     text_obs.delete("1.0","end")
     text_obs.insert(END, obs)
-    
-    
+        
 def clear():
-    global obs, comeco, fim
+    global obs, setor, fim, rodape
     obs = ""
-    comeco = ""
+    setor = ""
     fim = ""
+
+    rb_liberada.Checkbutton = False
+    rb_retida.Checkbutton = False
     atualizar()
+
+def ctrlc():
+    text = text_obs.get("1.0","end-1c")
+    pc.copy(text)
 
 def liberada():
     global fim
@@ -33,31 +47,40 @@ window = Tk()
 
 window.title("Análise do N2")
 
-#texto_orientacao = Label(window, text="Clique no botão para ver as cotações das moedas. ")
-#texto_orientacao.grid(column=0, row=0, padx=10, pady=10)
-#
+
+# LABEL AND ENTRY - USER
+
+lb_user = Label(window, text="Usuário: ")
+lb_user.grid(row=0, column=0)
+ent_user = Entry(window)
+ent_user.grid(row=0, column=1)
+
+# TEXTO PRINCIPAL
+text_obs = Text(window, width = 60, height=10)
+text_obs.grid(column=0, row=1, columnspan=4)
+
+# RADIO BUTTONS - OS LIBERADA / RETIDA
 var = IntVar()
- 
+
 rb_retida = Radiobutton(window, text = "OS retida", variable = var, value = 1, command=retida)
-rb_retida.grid(column=0, row=2, padx = 5, pady = 5)
+rb_retida.grid(column=0, row=3, padx = 5, pady = 5)
  
 rb_liberada = Radiobutton(window, text = "OS liberada", variable = var, value = 2, command=liberada)
-rb_liberada.Checkbutton = False
-rb_liberada.grid(column=1, row=2, padx = 5, pady = 5)
-#
+rb_liberada.grid(column=1, row=3, padx = 5, pady = 5)
 
-#botao = Button(window, text="Ok", command=atualizar)
-#botao.grid(column=0, row=1, padx=10, pady=10)
-
+# BUTTON CLEAR
 bt_clear = Button(window, text = "Limpar", command=clear)
-bt_clear.grid(column=2, row=2, padx=10, pady=10)
+bt_clear.grid(column=2, row=3, padx=10, pady=10)
 
-text_obs = Text(window, width = 50, height=10)
-text_obs.grid(column=0, row=0, columnspan=3)
+# BUTTON COPY
+bt_copy = Button(window, text = "Copiar", command=ctrlc)
+bt_copy.grid(column=3, row=3, padx=10, pady=10)
 
-print(obs)
-print(fim)
+today = datetime.datetime.now()
 
+td = today.strftime("%d/%m/%Y - %H:%M")
 
+user = ent_user.get()
+rodape = f" - {td}\n"+("-"*51)
 
 window.mainloop()
